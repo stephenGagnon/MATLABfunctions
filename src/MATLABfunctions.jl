3fd3109c1780e77bc38c94ae13df23575b193b6c
Stephen Gagnon <srgagnon@buffalo.edu>
2021-09-02 13:43:39 -0400

    added function to create 'plot3' MATLAB plot added function to create a 3D scatterplot of PSO particles added function to plot the satellite geometry added function to create MATLAB 3D quiver plot

--- src/MATLABfunctions.jl
+++ src/MATLABfunctions.jl
@@ -1,4 +1,7 @@
 module MATLABfunctions
+using MATLAB
+
+export MATLABhistogram, plot3, MRPScatterPlot, plotSat, multiQuiverPlot3
 
 function MATLABHistogram(data,binNo)
 
@@ -12,8 +15,111 @@ function MATLABHistogram(data,binNo)
     """)
 end
 
-function MATLABPlot3()
+function plot3(p,c=nothing)
 
+    if c == nothing
+        @mput p
+
+        eval_string("""
+
+        f = figure
+        ax = gca
+        hold on
+        for i = 1:size(p,2)
+
+            plot3(ax,p(1,i),p(2,i),p(3,i),'*')
+
+        end
+        hold off
+
+        """)
+    else
+        @mput p
+        @mput c
+
+        eval_string("""
+
+        f = figure
+        ax = gca
+        hold on
+        for i = 1:size(p,2)
+
+            plot3(ax,p(1,i),p(2,i),p(3,i),'*','color',c(:,i))
+
+        end
+        hold off
+
+        """)
+    end
+end
+
+function MRPScatterPlot(p,c)
+
+    @mput p
+    @mput c
+
+    eval_string("""
+
+    f = figure
+    ax = gca
+    hold on
+    for i = 1:size(p,2)
+
+        plot3(ax,p(1,i),p(2,i),p(3,i),'*','color',c(:,i))
+
+    end
+    hold off
+
+    """)
+end
+
+function plotSat(obj, scen, A)
+
+    # if typeof(s) != MSession
+    #
+    # end
+    @mput obj
+    # put_variable(s, :obj, mxarray(obj))
+    @mput scen
+    # put_variable(s, :scen, mxarray(scen))
+    @mput A
+    # put_variables(s, :A, mxarray(A))
+
+
+    eval_string("""
+        addpath("/Users/stephengagnon/matlab/NASA");
+        sat = objectGeometry('facetNo',obj.facetNo,'Area',obj.Areas,'nu',obj.nu,'nv',obj.nv,...
+        'Rdiff',obj.Rdiff,'Rspec',obj.Rspec,'I',obj.J,'vertices',obj.vertices,...
+        'facetVerticesList',obj.vertList,'Attitude',A,'obsNo',scen.obsNo,'obsVecs',scen.obsVecs,...
+        'obsDist',scen.d,'sunVec',scen.sunVec,'C',scen.C);
+
+        sat = sat.plot()
+        """)
+end
+
+function multiQuiverPlot3(v,p,c=nothing)
+    
+    @mput v
+    @mput p
+
+    if c == nothing
+        eval_string("""
+        figure
+        hold on
+        for i = 1:size(v,2)
+            quiver3(p(1,i),p(2,i),p(3,i),v(1,i),v(2,i),v(3,i))
+        end
+        """)
+    else
+        @mput c
+        eval_string("""
+        figure
+        hold on
+        for i = 1:size(v,2)
+            quiver3(p(1,i),p(2,i),p(3,i),v(1,i),v(2,i),v(3,i),'color',c(:,i))
+        end
+        """)
+    end
 end
 
 end # module
