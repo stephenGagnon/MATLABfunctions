263f007c5cb628cce230e97369841f5fa8dc9fdf
Stephen Gagnon <srgagnon@buffalo.edu>
2022-07-27 13:40:37 -0500

    added plot_matlab and subplot_matlab functions to allow for usage of MATLAB plotting tools

--- src/MATLABfunctions.jl
+++ src/MATLABfunctions.jl
@@ -2,7 +2,7 @@ module MATLABfunctions
 using MATLAB
 # using Infiltrator
 
-export MATLABhistogram, plot3, MRPScatterPlot, plotSat, multiQuiverPlot3, closeAll, saveData, imagesc
+export MATLABhistogram, plot3, MRPScatterPlot, plotSat, multiQuiverPlot3, closeAll, saveData, imagesc, plot_MATLAB, subplot_MATLAB
 
 struct spaceScenario
     obsNo
@@ -12,7 +12,7 @@ struct spaceScenario
     obsVecs
 end
 
-function MATLABHistogram(data,binNo)
+function MATLABHistogram(data, binNo)
 
     @mput data
     @mput binNo
@@ -24,7 +24,7 @@ function MATLABHistogram(data,binNo)
     """)
 end
 
-function plot3(p,c=nothing)
+function plot3(p, c=nothing)
 
     if c == nothing
         @mput p
@@ -62,7 +62,7 @@ function plot3(p,c=nothing)
     end
 end
 
-function MRPScatterPlot(p,c)
+function MRPScatterPlot(p, c)
 
     @mput p
     @mput c
@@ -89,7 +89,7 @@ function plotSat(obj, scenI, A)
     # end
     if typeof(scenI.obsVecs) == Array{Array{Float64,1},1}
         obsVecsm = hcat(scenI.obsVecs...)
-        scen = spaceScenario(scenI.obsNo,scenI.C,scenI.d[:],scenI.sunVec,obsVecsm)
+        scen = spaceScenario(scenI.obsNo, scenI.C, scenI.d[:], scenI.sunVec, obsVecsm)
     else
         scen = scenI
     end
@@ -112,7 +112,7 @@ function plotSat(obj, scenI, A)
         """)
 end
 
-function multiQuiverPlot3(v,p,c=nothing)
+function multiQuiverPlot3(v, p, c=nothing)
 
     @mput v
     @mput p
@@ -137,7 +137,7 @@ function multiQuiverPlot3(v,p,c=nothing)
     end
 end
 
-function imagesc(x,y,M; xlabelstring = "x",ylabelstring = "y",titlestring = "",clim = "auto")
+function imagesc(x, y, M; xlabelstring="x", ylabelstring="y", titlestring="", clim="auto")
     @mput M
     @mput x
     @mput y
@@ -163,7 +163,7 @@ function closeAll()
     close all""")
 end
 
-function saveData(filename, varnames, data; path = "/Users/stephengagnon/matlab/storage/dataFromJulia/")
+function saveData(filename, varnames, data; path="/Users/stephengagnon/matlab/storage/dataFromJulia/")
 
     @mput filename
     @mput path
@@ -178,5 +178,94 @@ function saveData(filename, varnames, data; path = "/Users/stephengagnon/matlab/
     """)
 end
 
+function plot_MATLAB(t, x, varin...; title_string="", xlabel_string="", ylabel_string="", xlim_val=nothing, ylim_val=nothing, grid_check=false)
+    linespec = Array{Any,1}(undef, length(varin))
+    linespec[:] .= varin[:]
+    @mput t
+    @mput x
+    @mput linespec
+    @mput title_string
+    @mput xlabel_string
+    @mput ylabel_string
+    if !isnothing(xlim_val)
+        @mput xlim_val
+    end
+    if !isnothing(ylim_val)
+        @mput ylim_val
+    end
+    @mput grid_check
 
+    if !isnothing(xlim_val) && !isnothing(ylim_val)
+        eval_string("plot(t,x,linespec{:}); title(title_string); xlabel(xlabel_string); ylabel(ylabel_string); xlim(xlim_val); ylim(ylim_val); if grid_check; grid on; end")
+    elseif !isnothing(xlim_val)
+        eval_string("plot(t,x,linespec{:}); title(title_string); xlabel(xlabel_string); ylabel(ylabel_string); xlim(xlim_val); if grid_check; grid on; end")
+    elseif !isnothing(ylim_val)
+        eval_string("plot(t,x,linespec{:}); title(title_string); xlabel(xlabel_string); ylabel(ylabel_string); ylim(ylim_val); if grid_check; grid on; end")
+    elseif isnothing(xlim_val) && isnothing(ylim_val)
+        eval_string("plot(t,x,linespec{:}); title(title_string); xlabel(xlabel_string); ylabel(ylabel_string); if grid_check; grid on; end")
+    end
+end
+
+function subplot_MATLAB(t, x, linespec; title_string=emptyStringArray(size(t)), xlabel_string=emptyStringArray(size(t)), ylabel_string=emptyStringArray(size(t)), xlim_val="no value", ylim_val="no value", grid_check=Array{Bool,length(size(t))}(undef, size(t)) .= false)
+
+    @mput t
+    @mput x
+    @mput linespec
+    @mput title_string
+    @mput xlabel_string
+    @mput ylabel_string
+    if !isnothing(xlim_val)
+        @mput xlim_val
+    end
+    if !isnothing(ylim_val)
+        @mput ylim_val
+    end
+    @mput grid_check
+
+    # eval_string("""
+    # save("/Users/stephengagnon/matlab/storage/dataFromJulia/test",'t','x','linespec')
+    # """)
+
+    eval_string(
+        raw" count = 1;
+        [n,m] = size(t);
+        f = figure;
+        for i = 1:n
+            for j = 1:m
+                subplot(n,m,count)
+                hold on
+                
+                for k = 1:length( x{i,j} )
+                    plot(t{i,j}, x{i,j}{k} , linespec{i,j}{k}{:})
+                end
+    
+                title(title_string{i,j}); 
+                xlabel(xlabel_string{i,j}); 
+                ylabel(ylabel_string{i,j});
+    
+                if class(xlim_val) == 'cell'
+                    xlim(xlim_val{i,j});
+                end
+                if class(ylim_val) == 'cell'
+                    ylim(ylim_val{i,j});
+                end
+    
+                if grid_check(i,j)
+                        grid on
+                end
+    
+                hold off
+                count = count + 1;
+            end
+        end
+        set(f,'position',[1 1 1792 1016])
+    
+        ")
+end
+
+function emptyStringArray(dims)
+    out = Array{String,length(dims)}(undef, dims)
+    out .= ""
+    return out
+end
 end # module
